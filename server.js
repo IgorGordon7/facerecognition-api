c608eeb9eac9d6ea7a743d76d371df02b965129e
Igorg Gordon <berkut1987@gmail.com>
2021-05-20 18:03:17 +0300

    Adding transactions between 2 tables(users and logins) encrypting password(not storing them,only hashes) and finishing to Set up the server

--- server.js
+++ server.js
@@ -18,74 +18,67 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password:'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password:'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login:[
-        {
-            id:'1231',
-            hash:'',
-            email:'john@gmail.comn'
-        }
-    ]
-}
 
-app.get('/',(req,res) =>{
-    res.send(database.users)
-})
+// app.get('/',(req,res) =>{
+//     res.send(database.users)
+// })
 
-app.get('/profile/:id',(req,res) =>{
+app.get('/profile/:id', (req, res) => {
     const {id} = req.params
-   db.select('*').from('users').where({id:id}).then(user =>{
-       if(user.length){
-           res.json(user[0])
-       }else{
-           res.status(400).json('User not found')
-       }
-   })
+    db.select('*').from('users').where({id: id}).then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('User not found')
+        }
+    })
        .catch(err => res.status(400).json("Error getting user"))
 })
 
-app.post('/signin',(req,res) =>{
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    }else{
-        res.status(400).json('Error Login In')
-    }
-    res.json('Signing')
+app.post('/signin',(req,res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('Unable to get user'))
+            } else {
+                res.status(400).json('Wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('Wrong credentials'))
 })
 
-app.post('/register',(req,res) =>{
-    const {name,email,password} = req.body
-    bcrypt.hash(password,null,null,function (err,hash){
-
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body
+    const hash = bcrypt.hashSync(password)
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    }).then(user => {
+                        res.json(user[0])
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
     })
-    db('users')
-        .returning('*')
-        .insert({
-        name:name,
-        email:email,
-        joined: new Date()
-    }).then(user =>{
-        res.json(user[0])
-    })
-        .catch(err =>{
+        .catch(err => {
             res.status(400).json('Unable to Register')
         })
 })
